3da609b40718b3cc1a1db8c09e8c7aa78cd59662
MRINMOY662 <ddas38192@gamil.com>
2023-08-21 17:25:55 +0530

    Update

--- SchoolApp/urls.py
+++ SchoolApp/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('', include('App.urls')),
 ]
 
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
