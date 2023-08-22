daac48f68f5355e18526de4834a555af8268e7d3
MRINMOY662 <ddas38192@gamil.com>
2023-08-22 14:49:29 +0530

    Update-1.1

--- SchoolApp/settings.py
+++ SchoolApp/settings.py
@@ -136,8 +136,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_URL = '/media/'
-MEDIA_URL = 'https://<schoolapp-f7483>.web.app/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
