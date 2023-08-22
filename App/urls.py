5f2f3fd7d9240b6700451cb808b2e374f9065533
MRINMOY662 <ddas38192@gamil.com>
2023-08-22 15:50:45 +0530

    Fixed media files configuration

--- App/urls.py
+++ App/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf import settings
 from . import views
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
     # path('home/', home)
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', views.home, name='home'),
     path('about/', views.about_view, name='about'),
     path('error-404/', views.error_404, name='error-404'),
